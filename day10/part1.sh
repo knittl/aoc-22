025494cf12add2957b7d076502819f6acc984d08
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2022-12-10 11:17:11 +0100

    Day 10, part 1 awk edition

--- day10/part1.sh
+++ day10/part1.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-while read -r op value; do
-	case "$op" in
-		noop) printf '0\n' ;;
-		addx)
-			printf '0\n'
-			printf '%d\n' "$value"
-			;;
-	esac
-done < input |
+awk '
+$1 == "noop" { print 0 }
+$1 == "addx" { print 0; print $2 }
+' input |
 	awk 'BEGIN{reg=1}{print NR, reg; reg+=$1}' |
 	sed -n "$(seq -s 'p;' 20 40 220)p" |
 	awk '{s+=$1*$2}END{print s}'
